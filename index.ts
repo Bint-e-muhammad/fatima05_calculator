e15ec09c463b93d851659641d149fc4953b653fa
Fatima Ali <160358652+Bint-e-muhammad@users.noreply.github.com>
2024-04-29 11:19:06 +0500

    Add files via upload
    This project implements a command line simple calculator application using TypeScript and Node.js. The calculator performs basic arithmetic operations such as addition, subtraction, multiplication, and division.

---
+++ index.ts
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import Inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  {
+    message: "Enter first number",
+    type: "number",
+    name: "firstNumber",
+  },
+  { message: "Enter second number", type: "number", name: "secondNumber" },
+  {
+    message: "select one of the operators to performs operation",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "subtraction", "Multiplication", "Division", "module"],
+  },
+]);
+// conditional statement..
+if (answer.operator === "Addition") {
+  console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operator === "subtraction") {
+  console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.operator === "Multiplication") {
+  console.log(answer.firstNumber * answer.secondNumber);
+} else if (answer.operator === "Division") {
+  console.log(answer.firstNumber / answer.secondNumber);
+}
+else if (answer.operator === "module") 
+    console.log(answer.firstNumber ** answer.secondNumber);
